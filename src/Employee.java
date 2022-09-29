66b32cf0e3537d0177fb0f9b57406e1e15e15663
chaitanya <chaitanya@localhost>
2022-09-28 20:34:50 -0400

    Added Create and View button along with the attributes in the back end code.

---
+++ src/Employee.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chaitanya
+ */
+public class Employee {
+    
+    private String Name;
+    private String Employee_ID;
+    private String Age;
+    private String Gender;
+    private String Start_Date;
+    private String Level;
+    private String Team_Info;
+    private String Position_Title;
+    private String Contact_Info;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmployee_ID() {
+        return Employee_ID;
+    }
+
+    public void setEmployee_ID(String Employee_ID) {
+        this.Employee_ID = Employee_ID;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStart_Date() {
+        return Start_Date;
+    }
+
+    public void setStart_Date(String Start_Date) {
+        this.Start_Date = Start_Date;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeam_Info() {
+        return Team_Info;
+    }
+
+    public void setTeam_Info(String Team_Info) {
+        this.Team_Info = Team_Info;
+    }
+
+    public String getPosition_Title() {
+        return Position_Title;
+    }
+
+    public void setPosition_Title(String Position_Title) {
+        this.Position_Title = Position_Title;
+    }
+
+    public String getContact_Info() {
+        return Contact_Info;
+    }
+
+    public void setContact_Info(String Contact_Info) {
+        this.Contact_Info = Contact_Info;
+    }
+
+    
+
+
+    
+    
+    
+    
+}
